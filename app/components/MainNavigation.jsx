c6c7fa4990081cd7188fb0c9e9bc2e13763805ff
Steven Hill <steven@stevenhill.co.za>
2023-06-21 12:02:16 +0200

    working with servers

---
+++ app/components/MainNavigation.jsx
@@ -0,0 +1,20 @@
+// The NavLink Component is similar to the Link Component
+// The difference is it has an active link class, which can be used to give it a special style
+import { NavLink } from '@remix-run/react';
+
+function MainNavigation() {
+  return (
+    <nav id="main-navigation">
+      <ul>
+        <li className="nav-item">
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/notes">My Notes</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default MainNavigation;
