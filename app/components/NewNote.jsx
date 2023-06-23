f81bbafcc42666c5c26cbef869ed96ec1ba9ce24
Steven Hill <steven@stevenhill.co.za>
2023-06-23 09:26:33 +0200

    Disabling submit when submitting

--- app/components/NewNote.jsx
+++ app/components/NewNote.jsx
@@ -1,7 +1,20 @@
-import { Form } from '@remix-run/react';
+import { Form, useNavigation } from '@remix-run/react';
 import styles from './NewNote.css';
 
 function NewNote() {
+  // The useNavigation hook contains useful data about ongoing requests that may be happening behind the scenes
+  const navigation = useNavigation();
+
+  // Example: we can access the state property to see whether we are submitting data/loading data/not doing anything
+  const isSubmitting = navigation.state === 'submitting';
+
+  // Example: submission property to get more info on the latest form submission
+  // Like the path where the request was sent; what the method was; or what kind of data was submitted
+  // navigation.submission
+
+  // Example: can use the type prop to see if you were redirected
+  // navigation.type
+
   return (
     <Form method="post" id="note-form">
       <p>
@@ -13,7 +26,11 @@ function NewNote() {
         <textarea id="content" name="content" rows="5" required />
       </p>
       <div className="form-actions">
-        <button>Add Note</button>
+        {/* We are adding the isSubmitting prop to tell React to disable when submitting */}
+        <button disabled={isSubmitting}>
+          {/* Using isSubmitting to change the text depending on the state */}
+          {isSubmitting ? 'Adding...' : 'Add Note'}
+        </button>
       </div>
     </Form>
   );
