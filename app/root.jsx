07652c7aafa0509c592f2688b16ee9b622932d1c
Steven Hill <steven@stevenhill.co.za>
2023-06-28 10:47:34 +0200

    Setting up ErrorBoundary

--- app/root.jsx
+++ app/root.jsx
@@ -13,6 +13,35 @@ import MainNavigation from './components/MainNavigation';
 
 import styles from './styles/main.css';
 
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        {/* Used to inject any metadata */}
+        <Meta />
+        {/* Inject any links (using the Remix Link Components) applying to pages */}
+        <Links />
+      </head>
+      <body>
+        <header>
+          {/* Setting MainNavigation here to target ALL routes/pages */}
+          <MainNavigation />
+        </header>
+        {/* Where your pages get called to render to screen */}
+        <Outlet />
+        {/* Restoring the scrollbar position if users navigate between pages */}
+        <ScrollRestoration />
+        {/* Injecting client side scripts when page is downloaded on the client side */}
+        <Scripts />
+        {/* Utility component to get live reload */}
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
 // The error prop is the default JS error object, and will have a message property we can access
 export const ErrorBoundary = ({ error }) => {
   <html lang="en">
@@ -52,32 +81,3 @@ export const links = () => [
     ? [{ rel: 'stylesheet', href: cssBundleHref }]
     : [{ rel: 'stylesheet', href: styles }]),
 ];
-
-export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        {/* Used to inject any metadata */}
-        <Meta />
-        {/* Inject any links (using the Remix Link Components) applying to pages */}
-        <Links />
-      </head>
-      <body>
-        <header>
-          {/* Setting MainNavigation here to target ALL routes/pages */}
-          <MainNavigation />
-        </header>
-        {/* Where your pages get called to render to screen */}
-        <Outlet />
-        {/* Restoring the scrollbar position if users navigate between pages */}
-        <ScrollRestoration />
-        {/* Injecting client side scripts when page is downloaded on the client side */}
-        <Scripts />
-        {/* Utility component to get live reload */}
-        <LiveReload />
-      </body>
-    </html>
-  );
-}
