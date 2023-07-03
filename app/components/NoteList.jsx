93fea3900d9c6f65cc37f7dbebe1899de36d33ac
Steven Hill <steven@stevenhill.co.za>
2023-07-03 14:34:50 +0200

    End of essentials

--- app/components/NoteList.jsx
+++ app/components/NoteList.jsx
@@ -1,3 +1,5 @@
+import { Link } from '@remix-run/react';
+
 import styles from './NoteList.css';
 
 function NoteList({ notes }) {
@@ -5,26 +7,31 @@ function NoteList({ notes }) {
     <ul id="note-list">
       {notes.map((note, index) => (
         <li key={note.id} className="note">
-          <article>
-            <header>
-              <ul className="note-meta">
-                <li>#{index + 1}</li>
-                <li>
-                  <time dateTime={note.id}>
-                    {new Date(note.id).toLocaleDateString('en-US', {
-                      day: 'numeric',
-                      month: 'short',
-                      year: 'numeric',
-                      hour: '2-digit',
-                      minute: '2-digit',
-                    })}
-                  </time>
-                </li>
-              </ul>
-              <h2>{note.title}</h2>
-            </header>
-            <p>{note.content}</p>
-          </article>
+          {/* The Link Component supports RELATIVE paths. This means if you type 'note-1': note-1 will appended to the currently active path i.e. localhost:3000/notes/note-1
+          If you were to add a slash at the beginning of the path, it will change to an ABSOLUTE path
+           */}
+          <Link to={'/' + note.id}>
+            <article>
+              <header>
+                <ul className="note-meta">
+                  <li>#{index + 1}</li>
+                  <li>
+                    <time dateTime={note.id}>
+                      {new Date(note.id).toLocaleDateString('en-US', {
+                        day: 'numeric',
+                        month: 'short',
+                        year: 'numeric',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                      })}
+                    </time>
+                  </li>
+                </ul>
+                <h2>{note.title}</h2>
+              </header>
+              <p>{note.content}</p>
+            </article>
+          </Link>
         </li>
       ))}
     </ul>
---
+++ app/routes/notes.$noteId.jsx
@@ -0,0 +1,50 @@
+import { json } from '@remix-run/node';
+import { Link, useLoaderData } from '@remix-run/react';
+
+import { getStoredNotes } from '~/data/notes';
+import styles from '~/styles/note-details.css';
+
+export default function NoteDetailsPage() {
+  const note = useLoaderData();
+
+  return (
+    <main id="note-details">
+      <header>
+        <nav>
+          <Link to="/notes">Back to all Notes</Link>
+        </nav>
+        <h1>{note.title}</h1>
+      </header>
+      <p id="note-details-content">{note.content}</p>
+    </main>
+  );
+}
+
+export async function loader({ params }) {
+  const notes = await getStoredNotes();
+  const noteId = params.noteId;
+
+  const selectedNote = notes.find((note) => note.id === noteId);
+
+  // Setting up server error response
+  if (!selectedNote) {
+    throw json(
+      { message: 'Could not find note for id ' + noteId },
+      { status: 404 }
+    );
+  }
+
+  return selectedNote;
+}
+
+// We are exporting from the links function so we can reuse the stylesheet for note-details.css
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
+}
+
+export function meta({ data }) {
+  return {
+    title: data.title,
+    description: 'Manage your notes with ease.',
+  };
+}
